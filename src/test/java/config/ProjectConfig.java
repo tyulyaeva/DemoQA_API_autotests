60cc7f28d44cda4e0885ba6e5026328e978fe9f8
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-07-09 12:01:24 +0300

    HW_19

---
+++ src/test/java/config/ProjectConfig.java
@@ -0,0 +1,35 @@
+package config;
+import com.codeborne.selenide.Configuration;
+import io.restassured.RestAssured;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class ProjectConfig {
+    private final WebConfig webConfig;
+
+    public ProjectConfig(WebConfig webConfig) {
+        this.webConfig = webConfig;
+    }
+
+    public void setConfig() {
+        RestAssured.baseURI = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = webConfig.getBaseUrl();
+        Configuration.browser = webConfig.getBrowser().toString();
+        Configuration.browserVersion = webConfig.getBrowserVersion();
+        Configuration.browserSize = webConfig.getBrowserSize();
+
+        if (webConfig.isRemote()) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true,
+                    "name", "Test: " + UUID.randomUUID()
+            ));
+            Configuration.remote = webConfig.getRemoteUrl();
+            Configuration.browserCapabilities = capabilities;
+        }
+    }
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,51 +3,26 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attachments;
 import io.qameta.allure.selenide.AllureSelenide;
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import java.util.Map;
-import java.util.UUID;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import config.ProjectConfig;
+import config.WebConfig;
+import config.ConfigReader;
 
 public class TestBase {
-    private static final String SELENOID_URL = System.getProperty("selenoid.url");
-    private static final String SELENOID_LOGIN = System.getProperty("selenoid.login");;
-    private static final String SELENOID_PASSWORD = System.getProperty("selenoid.password");
+    public static final WebConfig webConfig = ConfigReader.getWebConfig();
+
     public static String URL_login = "/Account/v1/Login";
     public static String URL_book = "/BookStore/v1/Books";
     public static String URL_books = "/BookStore/v1/Book";
     public static String UPDATE_token = "/Account/v1/GenerateToken";
 
     @BeforeAll
-    public static void setupGlobalTestConfiguration() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
-        RestAssured.baseURI = "https://demoqa.com";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browser.version", "");
-        Configuration.browserSize = System.getProperty("browser.size", "1920x1080");
-
-        if (isRemoteExecution()) {
-            setupRemoteWebDriver();
-        }
-    }
-
-    private static boolean isRemoteExecution() {
-        return SELENOID_URL != null && !SELENOID_URL.isEmpty();
-    }
-
-    private static void setupRemoteWebDriver() {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true,
-                "name", "Test: " + UUID.randomUUID()
-        ));
-        Configuration.remote = "https://" + SELENOID_LOGIN + ":" + SELENOID_PASSWORD + "@" + SELENOID_URL + "/wd/hub";
-        Configuration.browserCapabilities = capabilities;
+    public static void setupBaseTestConfiguration() {
+        ProjectConfig projectConfig = new ProjectConfig(webConfig);
+        projectConfig.setConfig();
     }
 
     @BeforeEach
@@ -61,7 +36,7 @@ public class TestBase {
         Attachments.pageSource();
         if (!Configuration.browser.equals("firefox"))
             Attachments.browserConsoleLogs();
-        if (isRemoteExecution()) {
+        if (webConfig.isRemote()) {
             Attachments.addVideo();
         }
         closeWebDriver();
